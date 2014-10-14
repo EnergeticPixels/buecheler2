b034dff942b1f823f123a88efa0c7d1fd257ec90
Anthony Jackman <energeticpixels@gmail.com>
2014-10-13 23:08:27 -0500

    populating using info through ajax back to db. added added code to global.js (jquery section of site). modified index.jade to include section with specific user data.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
     // Populate the user table on initial page load
     populateTable();
 
+    // username link click
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
 });
 
 // Functions =============================================================
@@ -19,6 +22,9 @@ function populateTable() {
 
     // jQuery AJAX call for JSON
     $.getJSON( '/users/userlist', function( data ) {
+
+        // stick our user data array into a userList
+        userListData = data;
 
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
@@ -33,3 +39,30 @@ function populateTable() {
         $('#userList table tbody').html(tableContent);
     });
 };
+
+function showUserInfo(event) {
+
+    // present link from firing
+    event.preventDefault();
+
+    // retrieve username from link rel attribute
+    var thisUserName = $(this).attr('rel');
+
+    // Using .map to apply a function to each object in our userListData aray. This will spit out a brand new array
+    // containing only whatever the function name returns. That function (anonymus callback) strictly return
+    // the username.  If the original array contained two complete user objects, the the array returned by use
+    // of .map would only contain usernames. Then when we have that array of usernames, we are chaining indexOf
+    // in combination with the username of choice, to get the array index of that username. 
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+    // Use that index number stored as arrayposition to go back to our original user data array and 
+    // start pulling data.
+    var thisUserObject = userListData[arrayPosition];
+
+    // using the thisUserObject variable, populate info box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+
+}
