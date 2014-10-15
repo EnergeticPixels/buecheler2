1f0cfd0729b0a58887e1702ee95a3a95cbfd8295
Anthony Jackman <energeticpixels@gmail.com>
2014-10-14 19:03:32 -0500

    added to global script to handle adding users, added to jade lelements to post info, added a commented port to www for testing here at work

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,17 @@ router.get('/userlist', function (req, res) {
 	});
 });
 
+
+/* 
+ * POST userList
+ * just posting some ata (req.body) and your going to insert it into collection in database. if that goes well,
+ * return an empty string. if somethign goes haywire, return the error message that the database gives.
+*/
+router.post('/adduser', function (req, res) {
+	var db = req.db;
+	db.collection('userlist').insert(req.body, function (err, result) {
+		res.send((err === null) ? (msg: '') : (msg: err));
+	});
+});
+
 module.exports = router;
