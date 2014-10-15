dcde42a57e23dde05fd00b6c6e6ad8ac3dab4c44
Anthony Jackman <energeticpixels@gmail.com>
2014-10-15 15:59:29 -0500

    fixed an unclosed function in users, fixed space mixed with tabs in index.jade

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,9 @@ router.get('/userlist', function (req, res) {
 router.post('/adduser', function (req, res) {
 	var db = req.db;
 	db.collection('userlist').insert(req.body, function (err, result) {
-		res.send((err === null) ? (msg: '') : (msg: err));
+		res.send(
+			(err === null) ? { msg: '' } : { msg: err }
+		);
 	});
 });
 
